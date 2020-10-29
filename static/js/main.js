c9f121211c18156907a5a47be79af7bcf69c8996
david connaughton <davidconnaughton@davids-MacBook-Pro.local>
2020-10-29 15:27:36 +0000

    Added Categories Dropdown

--- static/js/main.js
+++ static/js/main.js
@@ -4,6 +4,7 @@ $(document).ready(function () {
     });
     $('.collapsible').collapsible();
     $('.tooltipped').tooltip();
+    $('select').formSelect();
     $('.datepicker').datepicker({
         format: "dd mm yyyy",
         yearRange: 3,
